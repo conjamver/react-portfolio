ffb9c9e843b089212dfc41511ef3cbfde35de9e2
conjamver <connor.j.vernon97@gmail.com>
2020-07-23 20:47:30 +1000

    Updates

---
+++ precache-manifest.9b60a7bc4aa1e908ad0fe9e2deabcb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a61cf5bb0f76b36befcdc31e036af7",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "c7a6de5c167d77fa114e",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/react-test/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a6de5c167d77fa114e",
+    "url": "/react-test/static/js/main.1fc7636e.chunk.js"
+  },
+  {
+    "revision": "327b644da23f7299988f",
+    "url": "/react-test/static/js/runtime-main.b5085482.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
